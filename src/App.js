59ea8fbe5aa3782bae24780e047ba88251735d50
elisa-daria <elisa.daria.tomasi@gmail.com>
2024-01-30 17:07:00 +0100

    adds Welcome

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import EpibooksNavbar from "./components/EpibooksNavbar";
 import EpibooksFooter from "./components/EpibooksFooter.jsx";
 import AllTheBooks from "./components/AllTheBooks.jsx";
+import Welcome from "./components/Welcome.jsx";
 
 function App() {
   return (
     <>
       <header>
         <EpibooksNavbar />
+        <Welcome />
       </header>
       <main className="bg-dark">
         <AllTheBooks />
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,10 @@
+import Alert from "react-bootstrap/Alert";
+
+const Welcome = () => {
+  return (
+    <Alert className="text-center" variant="warning">
+      EPIBOOKS: your favourite HORROR library!
+    </Alert>
+  );
+};
+export default Welcome;
