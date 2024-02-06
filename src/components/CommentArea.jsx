46660bc61c3d6a203c161d3e339d103e2ec88818
elisa-daria <elisa.daria.tomasi@gmail.com>
2024-02-06 16:50:52 +0100

    handles error and loading

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -4,17 +4,23 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import CommentList from "./CommentList";
 import AddComment from "./AddComment";
+import Error from "./Error";
+import Loading from "./Loading";
 
 const CommentArea = ({ asin }) => {
   // state = {
   //   comments: [],
   // };
   const [comments, setComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
   const myKey =
     "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWFhNTZjYjE4N2U1YzAwMTgxNGM2YjciLCJpYXQiOjE3MDY3OTI0NzUsImV4cCI6MTcwODAwMjA3NX0.Hrxk_4QSLfOqFfE70sKM0FUN9n2w6Ry5851PhmwdMKU";
   const myURL = "https://striveschool-api.herokuapp.com/api/comments/";
 
   const fetchingComments = (asin) => {
+    setIsLoading(true);
     fetch(myURL + asin, {
       headers: {
         Authorization: myKey,
@@ -30,9 +36,11 @@ const CommentArea = ({ asin }) => {
       .then((arrComments) => {
         console.log("comments", arrComments);
         setComments(arrComments);
+        setIsLoading(false);
       })
       .catch((err) => {
         alert("ERRORE:" + err);
+        setIsError(true);
       });
   };
   // componentDidMount() {
@@ -48,6 +56,8 @@ const CommentArea = ({ asin }) => {
   return (
     <Container className="bg-warning rounded-2 p-3 my-3">
       <Row className="justify-content-center">
+        {isLoading && <Loading />}
+        {isError && <Error />}
         <Col xs={12} xl={6}>
           <CommentList reviews={comments} />
         </Col>
---
+++ src/components/Error.jsx
@@ -0,0 +1,5 @@
+import Alert from "react-bootstrap/Alert";
+
+const Error = () => <Alert variant="info">Errore - ❌</Alert>;
+
+export default Error;
